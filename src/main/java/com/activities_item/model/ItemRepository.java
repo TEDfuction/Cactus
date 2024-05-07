dbe83fb5d8d9745e6f10e5619a4ba34b54f71da0
shengyang <hh870150@gmail.com>
2024-05-07 16:12:18 +0800

    activityItem

---
+++ src/main/java/com/activities_item/model/ItemRepository.java
@@ -0,0 +1,16 @@
+package com.activities_item.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface ItemRepository extends JpaRepository<ItemVO, Integer>{
+	    //建立交易
+		@Transactional
+		//修改操作
+		@Modifying
+		//刪除特定活動ID
+		@Query(value = "delete from activity_item where activity_id = ?1", nativeQuery = true)
+		void deleteByActivityId(int activityId);
+}
---
+++ src/main/java/com/activities_item/model/ItemService.java
@@ -0,0 +1,42 @@
+package com.activities_item.model;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("itemService")
+public class ItemService {
+	
+	@Autowired
+	ItemRepository itemRepository;
+	
+//	@Autowired
+//	private SessionFactory sessionFactory;
+	
+	public void addItem(ItemVO itemVO) {
+		itemRepository.save(itemVO);
+	}
+	
+	public void updateItem(ItemVO itemVO) {
+		itemRepository.save(itemVO);
+	}
+	public void deleteItem(Integer activityId) {
+		if(itemRepository.existsById(activityId))
+			itemRepository.deleteByActivityId(activityId);
+	}
+	public ItemVO getOneItem(Integer activityId) {
+		Optional<ItemVO> optional = itemRepository.findById(activityId);
+		return optional.orElse(null);// public T orElse(T other) : 如果值存在就回傳其值，否則回傳other的值
+	}
+	public List<ItemVO> getAll(){
+
+        return itemRepository.findAll();
+    }
+//	public List<ItemVO> getAll(Map<String, String[]> map){
+//		return Item_Compositegory.getAllItemVOs(map, sessionFactory.openSession());
+//	}
+	
+
+}
