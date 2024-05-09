dc29838ad1ecad42c4d39eb4101432d6e941974a
David <40817david@gmail.com>
2024-05-09 15:09:41 +0800

    商品購物車測試

--- src/main/java/com/shoporderdetail/model/ShopOrderDetailService.java
+++ src/main/java/com/shoporderdetail/model/ShopOrderDetailService.java
@@ -3,11 +3,16 @@ package com.shoporderdetail.model;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Component;
+@Component
 public class ShopOrderDetailService {
 
 	@Autowired
 	ShopOrderDetailRepository repository;
+	
+	public void addShopOrderDetail(ShopOrderDetailVO shopOrderDetail) {
+		repository.save(shopOrderDetail);
+	}
 	
 	public List<ShopOrderDetailVO> findByShopOrderId(Integer shopOrderId){
 		return repository.findByShopOrderId(shopOrderId);
