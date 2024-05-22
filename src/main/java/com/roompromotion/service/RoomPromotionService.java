9133a5ac86075455eb293a4ebb30c2726c75df42
Frey <rtysnow@gmail.com>
2024-05-22 16:45:49 +0800

    16:43 room整合

--- src/main/java/com/roompromotion/service/RoomPromotionService.java
+++
@@ -1,8 +0,0 @@
-//package com.roompromotion.service;
-//
-//import java.time.LocalDate;
-//import java.util.List;
-//
-//public interface RoomPromotionService {
-//    List<String> findByCheckInDate(LocalDate selectCheckIn);
-//}
